851dd24f91f16113ceda17f601b8f4c98d761e39
coinop-logan <coinop.logan@gmail.com>
2024-11-11 16:55:34 +0200

    Rearrange code for better read flow

--- src/Start.sol
+++ src/Start.sol
@@ -7,6 +7,11 @@ import "@ds-math/math.sol";
 
 import {console} from "forge-std/console.sol";
 
+/*
+    MockPriceOracle is a simple price oracle that returns a constant price for ETH.
+    Obviously not suitable for production.
+*/
+
 contract MockPriceOracle {
     address public owner;
 
@@ -26,19 +31,39 @@ contract MockPriceOracle {
     }
 }
 
+/*
+    Stable is a simple stablecoin protocol.
+    Interest is accrued per second. The interest rate can be changed by the owner at any time.
+    A global interest index is used to calculate the accrued debt on a position at a given time.
+
+    Liquidators can liquidate undercollateralized positions.
+    They are incentivized to do so for any position that has a collateral ratio above 1.0x.
+    Thus this protocol becomes unstable and likely insolvent if the price of ETH falls so quickly
+    that positions begin to fall below 1.0x collateral ratio before liquidators catch on.
+    Gas costs of liquidators make this even worse.
+    I am considering this issue out of scope, but some ideas:
+    - Adjust POSITION_LIQUIDATE_LTV to be higher.
+    - Remove/lower barriers to entry to liquidators, i.e. build good dashboards etc.
+    - A minimum position size could be enforced to mitigate gas cost concerns for liquidators.
+*/
 contract Stable is ERC20, Ownable, DSMath {
     MockPriceOracle priceOracle;
 
+    // A user can take debt as long as the collateral is valued at this ratio of the debt or above
     uint constant POSITION_TAKELOAN_LTV = 1.5e18; // 1.5
+    // A liquidator can liquidate a position if the collateral is valued at this ratio of the debt or below
     uint constant POSITION_LIQUIDATE_LTV = 1.1e18; // 1.1
 
     uint public interestRatePerSecond_ray;
+    // The global interest index, which is used to calculate the interest accrued on a position
     uint public globalInterestIndex_wad;
+    // The timestamp of the last interest rate change
     uint public lastInterestChangeTimestamp;
 
     struct Position {
         uint collateral;
         uint debt;
+        // The interest index at the last update, used to calculate the interest accrued on a position at a given time
         uint interestIndexAtLastUpdate_wad;
     }
 
@@ -62,6 +87,8 @@ contract Stable is ERC20, Ownable, DSMath {
         lastInterestChangeTimestamp = block.timestamp;
     }
 
+    // --- OWNER FUNCTIONS ---
+
     function updateInterestRate(uint _interestRatePerSecond_ray) external onlyOwner {
         accrueGlobalInterest();
 
@@ -69,35 +96,7 @@ contract Stable is ERC20, Ownable, DSMath {
         emit InterestRateUpdated(_interestRatePerSecond_ray);
     }
 
-    function accrueGlobalInterest() internal {
-        uint secondsElapsed = block.timestamp - lastInterestChangeTimestamp;
-
-        if (secondsElapsed > 0) {
-            globalInterestIndex_wad = calcCurrentGlobalInterestIndex_wad();
-
-            lastInterestChangeTimestamp = block.timestamp;
-        }
-    }
-
-    function calcCurrentGlobalInterestIndex_wad() internal view returns (uint) {
-        uint secondsElapsed = block.timestamp - lastInterestChangeTimestamp;
-
-        return accrueInterest_wad(globalInterestIndex_wad, interestRatePerSecond_ray, secondsElapsed);
-    }
-
-    function getPositionDebtWithInterest(address user) public view returns (uint) {
-        Position storage userPosition = userPositions[user];
-
-        if (userPosition.debt == 0) {
-            return 0;
-        }
-
-        return userPosition.debt * calcCurrentGlobalInterestIndex_wad() / userPosition.interestIndexAtLastUpdate_wad;
-    }
-
-    function getPositionCollateral(address user) external view returns (uint) {
-        return userPositions[user].collateral;
-    }
+    // --- POSITION HOLDER FUNCTIONS ---
 
     function openPosition() external payable {
         require(msg.value > 0, "Deposit must be greater than 0");
@@ -111,12 +110,6 @@ contract Stable is ERC20, Ownable, DSMath {
         userPosition.interestIndexAtLastUpdate_wad = calcCurrentGlobalInterestIndex_wad();
 
         emit PositionOpened(msg.sender, msg.value);
-    }
-
-    function accrueUserInterest(address user) internal {
-        userPositions[user].debt = getPositionDebtWithInterest(user);
-        userPositions[user].interestIndexAtLastUpdate_wad = calcCurrentGlobalInterestIndex_wad();
-        // todo: the above results in two calls to calcCurrentGlobalInterestIndex_wad; code should be reorganized to avoid this.
     }
 
     function takeLoan(uint loanAmount) external {
@@ -135,12 +128,6 @@ contract Stable is ERC20, Ownable, DSMath {
 
         _mint(msg.sender, loanAmount);
         emit PositionBorrowed(msg.sender, loanAmount);
-    }
-
-    function _repayLoanFromMsgSender(address who, uint repayAmount) internal {
-        userPositions[who].debt -= repayAmount;
-        _burn(msg.sender, repayAmount);
-        emit PositionRepaid(who, repayAmount);
     }
 
     function repayLoan(uint repayAmount) public {
@@ -172,7 +159,8 @@ contract Stable is ERC20, Ownable, DSMath {
         emit PositionClosed(msg.sender);
     }
 
-    // Liquidator pays back the full debt and takes all collateral
+    // --- LIQUIDATOR FUNCTIONS ---
+
     function liquidatePosition(address user) external {
         Position storage userPosition = userPositions[user];
 
@@ -183,13 +171,6 @@ contract Stable is ERC20, Ownable, DSMath {
         uint requiredCollateral_debtDenominated = wmul(userPosition.debt, POSITION_LIQUIDATE_LTV);
         uint requiredCollateral = wdiv(requiredCollateral_debtDenominated, priceOracle.ethPrice());
 
-        // console.log("requiredCollateral_debtDenominated", requiredCollateral_debtDenominated);
-        // console.log("priceOracle.ethPrice()", priceOracle.ethPrice());
-        
-        // console.log("user debt", userPosition.debt);
-        // console.log("requiredCollateral", requiredCollateral);
-        // console.log("user collateral", userPosition.collateral);
-
         require(userPosition.collateral < requiredCollateral, "User is not undercollateralized");
 
         _repayLoanFromMsgSender(user, userPosition.debt);
@@ -198,6 +179,52 @@ contract Stable is ERC20, Ownable, DSMath {
         userPosition.collateral = 0;
 
         emit PositionLiquidated(user);
+    }
+
+    // --- INTERNAL FUNCTIONS ---
+
+    function accrueGlobalInterest() internal {
+        uint secondsElapsed = block.timestamp - lastInterestChangeTimestamp;
+
+        if (secondsElapsed > 0) {
+            globalInterestIndex_wad = calcCurrentGlobalInterestIndex_wad();
+
+            lastInterestChangeTimestamp = block.timestamp;
+        }
+    }
+
+    function accrueUserInterest(address user) internal {
+        userPositions[user].debt = getPositionDebtWithInterest(user);
+        userPositions[user].interestIndexAtLastUpdate_wad = calcCurrentGlobalInterestIndex_wad();
+        // todo: the above results in two calls to calcCurrentGlobalInterestIndex_wad; code should be reorganized to avoid this.
+    }
+
+    function _repayLoanFromMsgSender(address who, uint repayAmount) internal {
+        userPositions[who].debt -= repayAmount;
+        _burn(msg.sender, repayAmount);
+        emit PositionRepaid(who, repayAmount);
+    }
+
+    // --- PURE/VIEW FUNCTIONS ---
+
+    function calcCurrentGlobalInterestIndex_wad() internal view returns (uint) {
+        uint secondsElapsed = block.timestamp - lastInterestChangeTimestamp;
+
+        return accrueInterest_wad(globalInterestIndex_wad, interestRatePerSecond_ray, secondsElapsed);
+    }
+
+    function getPositionDebtWithInterest(address user) public view returns (uint) {
+        Position storage userPosition = userPositions[user];
+
+        if (userPosition.debt == 0) {
+            return 0;
+        }
+
+        return userPosition.debt * calcCurrentGlobalInterestIndex_wad() / userPosition.interestIndexAtLastUpdate_wad;
+    }
+
+    function getPositionCollateral(address user) external view returns (uint) {
+        return userPositions[user].collateral;
     }
 
     // taken and modified from https://github.com/wolflo/solidity-interest-helper/blob/master/contracts/Interest.sol#L63C5
